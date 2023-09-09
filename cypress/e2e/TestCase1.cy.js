d2218c8f5a7f98b8af31672de4b81f848a0eab17
Juan Sebastian Santamaria Palomino <juanssp14@hotmail.com>
2023-09-08 23:24:52 -0500

    Checkboxes test case completed and table test case started

--- cypress/e2e/TestCase1.cy.js
+++ cypress/e2e/TestCase1.cy.js
@@ -53,6 +53,57 @@ describe('My First Cypress Test Suite', {viewportHeight: 947, viewportWidth: 192
     it('Check/Uncheck all the elements and validate their status', () => {
         cy.xpath('//h5[text()="Elements"]').click();
         cy.get('#item-1').click();
+        //Get parent element and find another webelement from there
         cy.xpath('//span[text()="Home"]').parent().parent().find('button').click();
+        cy.xpath('//span[text()="Desktop"]').parent().parent().find('button').click();
+        cy.xpath('//span[text()="Documents"]').parent().parent().find('button').click();
+        cy.xpath('//span[text()="Downloads"]').parent().parent().find('button').click();
+        cy.xpath('//span[text()="WorkSpace"]').parent().parent().find('button').click();
+        cy.xpath('//span[text()="Office"]').parent().parent().find('button').click();
+        cy.get('#tree-node-home+span').click();
+        //For each - syntax in cypress to handle arrays of web elements
+        cy.get('.rct-checkbox svg').each(($element) => {
+            cy.wrap($element).should('have.class', 'rct-icon-check');
+        });
+        cy.get('#tree-node-home+span').click();
+        cy.get('.rct-checkbox svg').each(($element) => {
+            cy.wrap($element).should('have.class', 'rct-icon-uncheck');
+        });
+    });
+
+    it('Add a new employee to the table of employees', () => {
+        const name = "Blazy";
+        const lastName = "Dora";
+        const email = "blazy69@gmail.com";
+        const age = "22";
+        const salary = "0";
+        const department = "Psychology";
+        cy.xpath('//h5[text()="Elements"]').click();
+        cy.get('#item-3').click();
+        cy.get('#addNewRecordButton').click();
+        //Filling the form
+        cy.get('#firstName').type(name);
+        cy.get('#lastName').type(lastName);
+        cy.get('#userEmail').type(email);
+        cy.get('#age').type(age);
+        cy.get('#salary').type(salary);
+        cy.get('#department').type(department);
+        cy.get('#submit').click();
+        //Check the number of pages
+        cy.get('.-totalPages').then(($element) => {
+            if (($element.text().includes('1')) && ($element.text().length == 1)) {
+                //Case 1 page
+                cy.get('.rt-tr-group>div:not(.-padRow)').last().within(() => {
+                    cy.get('div').eq(0).should('have.text', name);
+                    cy.get('div').eq(1).should('have.text', lastName);
+                    cy.get('div').eq(2).should('have.text', age);
+                    cy.get('div').eq(3).should('have.text', email);
+                    cy.get('div').eq(4).should('have.text', salary);
+                    cy.get('div').eq(5).should('have.text', department);
+                })
+            } else {
+                //Case more than 1 page
+            }
+          });
     });
 })
