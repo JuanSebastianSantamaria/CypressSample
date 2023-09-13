2188f7d1e9758a2513030faa759a97eda602ff53
Juan Sebastian Santamaria Palomino <juanssp14@hotmail.com>
2023-09-12 23:32:54 -0500

    Table of users test case completed

--- cypress/e2e/tests/DemoQA.cy.js
+++ cypress/e2e/tests/DemoQA.cy.js
@@ -9,7 +9,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
     return false
-})
+});
 
 //Test cases for the page: https://demoqa.com/
 
@@ -17,14 +17,14 @@ describe('My First Cypress Test Suite', {viewportHeight: 947, viewportWidth: 192
 
     beforeEach(() => {
         cy.visit("https://demoqa.com/");
-    })
+    });
 
     it('Visits the ToolsQA Demo Page and click on a button', () => {
         cy.get(':nth-child(1) > :nth-child(1) > .avatar > svg').click();
         cy.get('#item-4').click();
         cy.get('.mt-4:nth-child(3) button').click();
         cy.get('#dynamicClickMessage').should('be.visible');
-    })
+    });
 
     it('Validate that links under the elements section are working', () => {
         cy.xpath('//h5[text()="Elements"]').click();
@@ -47,7 +47,7 @@ describe('My First Cypress Test Suite', {viewportHeight: 947, viewportWidth: 192
             cy.get('#linkResponse').should('have.text', 'Link has responded with staus 403 and status text Forbidden');
             cy.get('a').eq(8).click();
             cy.get('#linkResponse').should('have.text', 'Link has responded with staus 404 and status text Not Found');
-        })
+        });
     });
 
     it('Check/Uncheck all the elements and validate their status', () => {
@@ -72,38 +72,41 @@ describe('My First Cypress Test Suite', {viewportHeight: 947, viewportWidth: 192
     });
 
     it('Add a new employee to the table of employees', () => {
-        const name = "Blazy";
-        const lastName = "Dora";
-        const email = "blazy69@gmail.com";
-        const age = "22";
-        const salary = "0";
-        const department = "Psychology";
         cy.xpath('//h5[text()="Elements"]').click();
         cy.get('#item-3').click();
-        cy.get('#addNewRecordButton').click();
-        //Filling the form
-        cy.get('#firstName').type(name);
-        cy.get('#lastName').type(lastName);
-        cy.get('#userEmail').type(email);
-        cy.get('#age').type(age);
-        cy.get('#salary').type(salary);
-        cy.get('#department').type(department);
-        cy.get('#submit').click();
-        //Check the number of pages
-        cy.get('.-totalPages').then(($element) => {
-            if (($element.text().includes('1')) && ($element.text().length == 1)) {
-                //Case 1 page
+        //Read the fixture file to get the data
+        cy.fixture('users.json').then(users => {
+            const usersArray = users;
+            usersArray.forEach((user) => {
+                cy.get('#addNewRecordButton').click();
+                //Filling the form
+                cy.get('#firstName').type(user.userName);
+                cy.get('#lastName').type(user.lastName);
+                cy.get('#userEmail').type(user.email);
+                cy.get('#age').type(user.age);
+                cy.get('#salary').type(user.salary);
+                cy.get('#department').type(user.department);
+                cy.get('#submit').click();
+                //Check the number of pages
+                cy.get('.-totalPages').then(($totalPages) => {
+                    //Check the current page
+                    cy.get('.-pageJump>input').then(($currentPage) => {
+                        //Go to the last page
+                        for (let i = parseInt($currentPage.val()); i <  parseInt($totalPages.text()); i++) {
+                            cy.get('.-next button').click();
+                        }
+                    });
+                });
+                //Validate the new user added to the table
                 cy.get('.rt-tr-group>div:not(.-padRow)').last().within(() => {
-                    cy.get('div').eq(0).should('have.text', name);
-                    cy.get('div').eq(1).should('have.text', lastName);
-                    cy.get('div').eq(2).should('have.text', age);
-                    cy.get('div').eq(3).should('have.text', email);
-                    cy.get('div').eq(4).should('have.text', salary);
-                    cy.get('div').eq(5).should('have.text', department);
-                })
-            } else {
-                //Case more than 1 page
-            }
-          });
+                    cy.get('div').eq(0).should('have.text', user.userName);
+                    cy.get('div').eq(1).should('have.text', user.lastName);
+                    cy.get('div').eq(2).should('have.text', user.age);
+                    cy.get('div').eq(3).should('have.text', user.email);
+                    cy.get('div').eq(4).should('have.text', user.salary);
+                    cy.get('div').eq(5).should('have.text', user.department);
+                });
+            });
+        });
     });
-})
+});
