45fdeb4c978268e73ca49d28255e88f5f5048893
Juan Sebastian Santamaria Palomino <juanssp14@hotmail.com>
2023-09-18 23:18:06 -0500

    POM usage example

--- cypress/e2e/tests/DemoQA.cy.js
+++ cypress/e2e/tests/DemoQA.cy.js
@@ -3,7 +3,10 @@
  * https://toolsqa.com/cypress-tutorial/
  * https://docs.cypress.io/guides/end-to-end-testing/testing-your-app
  * https://www.tutorialspoint.com/cypress/cypress_plugins.htm
+ * https://docs.cypress.io/guides/references/best-practices
  */
+import buttonsPage from "../../pagesDemoQA/buttonsPage";
+import menuComponent from "../../pagesDemoQA/menuComponent";
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
@@ -19,15 +22,16 @@ describe('My First Cypress Test Suite', {viewportHeight: 947, viewportWidth: 192
         cy.visit("https://demoqa.com/");
     });
 
-    it('Visits the ToolsQA Demo Page and click on a button', () => {
-        cy.get(':nth-child(1) > :nth-child(1) > .avatar > svg').click();
-        cy.get('#item-4').click();
-        cy.get('.mt-4:nth-child(3) button').click();
-        cy.get('#dynamicClickMessage').should('be.visible');
+    it('Clicks on a button that shows a message', () => {
+        //POM usage example
+        menuComponent.elements.elementsCard().click();
+        menuComponent.elements.buttonsMenuOption().click();
+        buttonsPage.elements.clickMeButton().click();
+        buttonsPage.elements.dynamicMessage().should('be.visible');
     });
 
-    it('Validate that links under the elements section are working', () => {
-        cy.xpath('//h5[text()="Elements"]').click();
+    it('Validates that links under the elements section are working', () => {
+        menuComponent.elements.elementsCard().click();
         cy.get('#item-5').click();
         //List of webelements eq(index)
         cy.get('p a').eq(2).click();
@@ -50,8 +54,8 @@ describe('My First Cypress Test Suite', {viewportHeight: 947, viewportWidth: 192
         });
     });
 
-    it('Check/Uncheck all the elements and validate their status', () => {
-        cy.xpath('//h5[text()="Elements"]').click();
+    it('Checks/Unchecks all the elements and validate their status', () => {
+        menuComponent.elements.elementsCard().click();
         cy.get('#item-1').click();
         //Get parent element and find another webelement from there
         cy.xpath('//span[text()="Home"]').parent().parent().find('button').click();
@@ -71,41 +75,39 @@ describe('My First Cypress Test Suite', {viewportHeight: 947, viewportWidth: 192
         });
     });
 
-    it('Add a new employee to the table of employees', () => {
-        cy.xpath('//h5[text()="Elements"]').click();
+    it('Adds new employees to the table of employees', () => {
+        menuComponent.elements.elementsCard().click();
         cy.get('#item-3').click();
         //Read the fixture file to get the data
-        cy.fixture('users.json').then(users => {
-            const usersArray = users;
-            usersArray.forEach((user) => {
-                cy.get('#addNewRecordButton').click();
-                //Filling the form
-                cy.get('#firstName').type(user.userName);
-                cy.get('#lastName').type(user.lastName);
-                cy.get('#userEmail').type(user.email);
-                cy.get('#age').type(user.age);
-                cy.get('#salary').type(user.salary);
-                cy.get('#department').type(user.department);
-                cy.get('#submit').click();
-                //Check the number of pages
-                cy.get('.-totalPages').then(($totalPages) => {
-                    //Check the current page
-                    cy.get('.-pageJump>input').then(($currentPage) => {
-                        //Go to the last page
-                        for (let i = parseInt($currentPage.val()); i <  parseInt($totalPages.text()); i++) {
-                            cy.get('.-next button').click();
-                        }
-                    });
+        cy.fixture('users.json').as('usersArray');
+        cy.get('@usersArray').each((user) => {
+            cy.get('#addNewRecordButton').click();
+            //Filling the form
+            cy.get('#firstName').type(user.userName);
+            cy.get('#lastName').type(user.lastName);
+            cy.get('#userEmail').type(user.email);
+            cy.get('#age').type(user.age);
+            cy.get('#salary').type(user.salary);
+            cy.get('#department').type(user.department);
+            cy.get('#submit').click();
+            //Check the number of pages
+            cy.get('.-totalPages').then(($totalPages) => {
+                //Check the current page
+                cy.get('.-pageJump>input').then(($currentPage) => {
+                    //Go to the last page
+                    for (let i = parseInt($currentPage.val()); i <  parseInt($totalPages.text()); i++) {
+                        cy.get('.-next button').click();
+                    }
                 });
-                //Validate the new user added to the table
-                cy.get('.rt-tr-group>div:not(.-padRow)').last().within(() => {
-                    cy.get('div').eq(0).should('have.text', user.userName);
-                    cy.get('div').eq(1).should('have.text', user.lastName);
-                    cy.get('div').eq(2).should('have.text', user.age);
-                    cy.get('div').eq(3).should('have.text', user.email);
-                    cy.get('div').eq(4).should('have.text', user.salary);
-                    cy.get('div').eq(5).should('have.text', user.department);
-                });
+            });
+            //Validate the new user added to the table
+            cy.get('.rt-tr-group>div:not(.-padRow)').last().within(() => {
+                cy.get('div').eq(0).should('have.text', user.userName);
+                cy.get('div').eq(1).should('have.text', user.lastName);
+                cy.get('div').eq(2).should('have.text', user.age);
+                cy.get('div').eq(3).should('have.text', user.email);
+                cy.get('div').eq(4).should('have.text', user.salary);
+                cy.get('div').eq(5).should('have.text', user.department);
             });
         });
     });
