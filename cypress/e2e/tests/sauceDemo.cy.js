b4190ebfcae9210374d698d5529ff2bbbe2a56e4
Juan Sebastian Santamaria Palomino <juanssp14@hotmail.com>
2023-09-27 23:49:09 -0500

    Test cases to validate different sorting of products completed

--- cypress/e2e/tests/sauceDemo.cy.js
+++ cypress/e2e/tests/sauceDemo.cy.js
@@ -10,7 +10,9 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('Validate that the user should be able to sort the products', {viewportHeight: 947, viewportWidth: 1920}, () => {
     beforeEach(() => {
-        cy.fixture('sauceDemoUser.json').as('user');
+        cy.fixture('sauceDemoUser.json').as('user').then(() => {
+            cy.task('log', 'User data loaded successfully');
+        });
         // Sauce demo doesn't fire load event, so these tests should be run in headless mode in order to pass
         // Command: npm run cyrun -- --spec cypress/e2e/tests/sauceDemo.cy.js
         cy.visit("https://www.saucedemo.com/");
@@ -24,10 +26,48 @@ describe('Validate that the user should be able to sort the products', {viewport
         homePage.elements.sortDropdown().select('za');
         homePage.elements.productTitle()
             .each(($element) => {
-                productsList.push($element.text())
+                productsList.push($element.text());
             })
             .then(() => {
-                cy.wrap(productsList).should('deep.equal', [...productsList].sort().reverse())
+                cy.wrap(productsList).should('deep.equal', [...productsList].sort().reverse());
+            });
+    });
+
+    it('sort products from A to Z', () => {
+        let productsList = new Array();
+        homePage.elements.sortDropdown().select('az');
+        homePage.elements.productTitle()
+            .each(($element) => {
+                productsList.push($element.text());
+            })
+            .then(() => {
+                cy.wrap(productsList).should('deep.equal', [...productsList].sort());
+            });
+    });
+
+    it('sort products by price from low to high', () => {
+        let productsList = new Array();
+        homePage.elements.sortDropdown().select('lohi');
+        homePage.elements.productPrice()
+            .each(($element) => {
+                let price = parseFloat($element.text().slice(1));
+                productsList.push(price);
+            })
+            .then(() => {
+                cy.wrap(productsList).should('deep.equal', [...productsList].sort((a,b) => a-b));
+            });
+    });
+
+    it('sort products by price from high to low', () => {
+        let productsList = new Array();
+        homePage.elements.sortDropdown().select('hilo');
+        homePage.elements.productPrice()
+            .each(($element) => {
+                let price = parseFloat($element.text().slice(1));
+                productsList.push(price);
+            })
+            .then(() => {
+                cy.wrap(productsList).should('deep.equal', [...productsList].sort((a,b) => a-b).reverse());
             });
     });
 });
